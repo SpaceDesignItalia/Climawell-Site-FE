6179ec99d1d9802f9d9237f2ddee561a19f855a8
PabloMicrosoft <pabloernestocolumbiebertot@gmail.com>
2025-01-23 00:24:18 +0100

    api

--- src/API/API.ts
+++ src/API/API.ts
@@ -1,4 +1,4 @@
-export const API_URL_IMG = 'http://localhost:3005'
-export const API_WEBSOCKET_URL = 'http://localhost:3005'
-export const API_URL = 'http://localhost:3005/API/v1'
-export const API_IMAGE_URL = 'http://localhost:3005/uploads/ProductImages/'
+export const API_URL_IMG = 'https://api.climawell.net:3005'
+export const API_WEBSOCKET_URL = 'https://api.climawell.net:3005'
+export const API_URL = 'https://api.climawell.net/API/v1'
+export const API_IMAGE_URL = 'https://api.climawell.net/uploads/ProductImages/'
