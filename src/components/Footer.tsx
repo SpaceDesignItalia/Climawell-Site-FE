ef51ceed6833683ba4de645d21c5660913f97c1c
PabloMicrosoft <pabloernestocolumbiebertot@gmail.com>
2026-03-04 12:33:03 +0100

    tolti loghi

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,11 +16,7 @@ const navigation = [
         title: 'Hermann Saunier Duval',
         href: 'https://www.hermann-saunierduval.it',
       },
-      { title: 'Icma', href: 'https://www.icmaspa.it' },
       { title: 'Aerauliqa', href: 'https://www.aerauliqa.it' },
-      { title: 'Scavolini', href: 'https://www.scavolini.com' },
-
-      
     ],
   },
   {
